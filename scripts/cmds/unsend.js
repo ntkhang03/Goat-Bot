9750d69fdccd3a8bdba820b94f96bb7945f2e08f
ntkhang03 <nthanhkhang2003@gmail.com>
2021-10-29 15:38:01 +0700

    first commit

---
+++ scripts/cmds/unsend.js
@@ -0,0 +1,23 @@
+this.config = {    
+  name: "unsend",
+  version: "1.0.1",
+  author: {
+    name: "NTKhang", 
+    contacts: ""
+  },
+  cooldowns: 5,
+  role: 0,
+  shortDescription: "Gỡ tin nhắn của bot",
+  longDescription: "Gỡ tin nhắn của bot",
+  category: "info",
+  guide: "Reply tin nhắn của bot với nội dung {p}{n}"
+};
+
+module.exports = {
+  config: this.config,
+  start: async function({ message, api, event, args, globalGoat }) {
+    if (event.messageReply.senderID != globalGoat.botID) return message.reply('Không thể gỡ tin nhắn của người khác!!');
+	  if (event.type != "message_reply") return message.reply('Vui lòng reply tin nhắn của bot cần gỡ');
+	  return api.unsendMessage(event.messageReply.messageID);
+  }
+};
