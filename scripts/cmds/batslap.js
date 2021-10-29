86f8e36e2d823a3a79c66e823f1fab190ec757ca
ntkhang03 <nthanhkhang2003@gmail.com>
2021-10-29 23:50:07 +0700

    first commit

--- scripts/cmds/batslap.js
+++ scripts/cmds/batslap.js
@@ -1,6 +1,6 @@
 this.config = {    
   name: "batslap",
-  version: "1.0.0",
+  version: "1.0.1",
   author: {
     name: "NTKhang", 
     contacts: ""
@@ -8,7 +8,7 @@ this.config = {
   cooldowns: 5,
   role: 0,
   shortDescription: "batman slap",
-  longDescription: "batman slap mention or you",
+  longDescription: "batman slap user mention or you",
   category: "image",
   envGlobal: {
     tokenFacebook: "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662"
@@ -19,6 +19,7 @@ module.exports = {
   config: this.config,
   start: async function({ api, globalGoat, args, message, event, download }) {
     const fs = require("fs-extra");
+    const axios = require("axios");
     const { senderID, mentions } = event;
     const pathBatSlap = __dirname + `/cache/batslap${Date.now()}.jpg`;
     const { tokenFacebook } = globalGoat.configCommands.envGlobal;
@@ -28,7 +29,19 @@ module.exports = {
     }
     
     const targetId = Object.keys(mentions).length > 0 ? Object.keys(mentions)[0] : senderID;
-    await download(`https://goatbot.tk/taoanhdep/batslap?author=${linkAvatar(senderID)}&target=${linkAvatar(targetId)}`, pathBatSlap);
+    let imgBuffer;
+    try {
+      imgBuffer = (await axios.get(`https://goatbot.tk/taoanhdep/batslap?author=${linkAvatar(senderID)}&target=${linkAvatar(targetId)}`, {
+        responseType: "arraybuffer"
+      })).data;
+    }
+    catch(error) {
+      let err;
+      if (error.response) err = JSON.parse(error.response.data.toString());
+      else err = error;
+      return message.reply(`Đã xảy ra lỗi ${err.error} ${err.message}`);
+    }
+    fs.writeFileSync(pathBatSlap, Buffer.from(imgBuffer));
     message.reply({ attachment: fs.createReadStream(pathBatSlap)}, () => fs.unlinkSync(pathBatSlap));
   }
 };
