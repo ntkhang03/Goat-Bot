fe00dd9bade91946830afffe213b6bb564d4565c
ntkhang03 <nthanhkhang2003@gmail.com>
2022-01-21 18:20:27 +0700

    update command rules

--- scripts/cmds/rules.js
+++ scripts/cmds/rules.js
@@ -1,6 +1,6 @@
 this.config = {    
   name: "rules",
-  version: "1.0.2",
+  version: "1.0.3",
   author: {
     name: "NTKhang", 
     contacts: ""
@@ -28,7 +28,10 @@ module.exports = {
     const { senderID, threadID, messageID } = event;
     
     const type = args[0];
-    const dataOfThread = (await threadsData.getData(threadID)).data;
+    const threadData = await threadsData.getData(threadID);
+    const dataOfThread = threadData.data;
+    const adminIDs = threadData.adminIDs;
+    
     if (!dataOfThread.rules) dataOfThread.rules = [];
     const rulesOfThread = dataOfThread.rules;
     
@@ -36,9 +39,10 @@ module.exports = {
       let msg = "";
       let i = 1;
       for (const rules of rulesOfThread) msg += `${i++}. ${rules}\n`;
-      message.reply(msg || "Nhóm này chưa có bất kỳ nội quy nào");
+      message.reply(msg || "Hiện tại nhóm bạn chưa có bất kỳ nội quy nào, để thêm nội quy cho nhóm hãy sử dụng `rules add`");
     }
     else if (type == "add" || type == "-a") {
+      if (!adminIDs.includes(senderID)) return message.reply("Chỉ có quản trị viên nhóm mới có thể thêm nội quy");
       if (!args[1]) return message.reply("Vui lòng nhập nội dụng cho nội quy bạn muốn thêm");
       rulesOfThread.push(args.slice(1).join(" "));
       return await threadsData.setData(threadID, {
@@ -49,8 +53,10 @@ module.exports = {
       });
     }
     else if (type == "delete" || type == "-d" || type == "del") {
+      if (!adminIDs.includes(senderID)) return message.reply("Chỉ có quản trị viên nhóm mới có thể xóa nội quy");
       if (!args[1] || isNaN(args[1])) return message.reply("Vui lòng nhập số thứ tự của nội quy bạn muốn xóa");
       const rulesDel = rulesOfThread[parseInt(args[1])-1];
+      if (!rulesDel) return message.reply(`Không tồn tại nội quy thứ ${args[1]}, hiện tại nhóm bạn có ${rulesOfThread.length} nội quy`);
       rulesOfThread.splice(parseInt(args[1]) - 1, 1);
       return await threadsData.setData(threadID, {
         data: dataOfThread
@@ -60,6 +66,7 @@ module.exports = {
       });
     }
     else if (type == "remove" || type == "-r") {
+      if (!adminIDs.includes(senderID)) return message.reply("Chỉ có quản trị viên nhóm mới có thể xoá bỏ tất cả nội quy của nhóm");
       dataOfThread.rules = [];
       return await threadsData.setData(threadID, {
         data: dataOfThread
@@ -67,6 +74,16 @@ module.exports = {
         if (err) return message.reply("Đã xảy ra lỗi: " + err.name + "\n" + err.message);
         message.reply(`Đã xóa toàn bộ nội quy của nhóm`);
       });
+    }
+    else if (!isNaN(type)) {
+      const stt = parseInt(type);
+      const getRules = rulesOfThread[stt - 1];
+      if (!getRules) {
+        message.reply(`Không tồn tại nội quy thứ ${stt}, hiện tại nhóm bạn ${rulesOfThread.length == 0 ? `chưa có nội quy nào` : `có ${rulesOfThread.length} nội quy`}`);
+      }
+      else {
+        message.reply(`Nội quy thứ ${stt} của nhóm:\n${getRules}`);
+      }
     }
   }
 };
