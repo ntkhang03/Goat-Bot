6e3510bda798d2a6de8b1a7cdca45b1ad5be1a8f
ntkhang03 <nthanhkhang2003@gmail.com>
2022-03-26 22:31:06 +0700

    update createFuncMessage.js

--- bot/createFuncMessage.js
+++ bot/createFuncMessage.js
@@ -1,20 +1,9 @@
 module.exports = (event, api) => {
   return {
-    send: function(text, callback) {
-      if (callback && typeof callback == "function") return api.sendMessage(text, event.threadID, callback);
-      else return api.sendMessage(text, event.threadID);
-    },
-    reply: function(text, callback) {
-      if (callback && typeof callback == "function") return api.sendMessage(text, event.threadID, callback, event.messageID);
-      else return api.sendMessage(text, event.threadID, event.messageID);
-    },
-    unsend: function(messageID, callback) {
-      if (callback && typeof callback == "function") return api.unsendMessage(messageID, callback);
-      else return api.unsendMessage(messageID);
-    },
-    reaction: function(emoji, messageID) {
-      return api.setMessageReaction(emoji, function() {}, true);
-    }
+    send: (form, callback) =>  api.sendMessage(form, event.threadID, callback),
+    reply: (form, callback) => api.sendMessage(form, event.threadID, event.messageID),
+    unsend: (messageID, callback) => api.unsendMessage(messageID),
+    reaction: (emoji, messageID) => api.setMessageReaction(emoji, messageID, () => {}, true)
   };
   // Có thể phát sinh những giá trị khác trong handleEvents.js
   // Can generate other values in handleEvents.js
